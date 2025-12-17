31d1a573152735f5bdd7f8f012565ac83f9797e6
bala-amypo <bala@amypo.in>
2025-12-17 05:24:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Shipment.java
@@ -0,0 +1,99 @@
+
+
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "shipments")
+public class Shipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pickup_location_id", nullable = false)
+    private Location pickupLocation;
+
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "drop_location_id", nullable = false)
+    private Location dropLocation;
+
+    @Column(nullable = false)
+    private Double weightKg;
+
+    @Column(nullable = false)
+    private LocalDate scheduledDate;
+
+   
+    public Shipment() {}
+
+    
+    public Shipment(Vehicle vehicle, Location pickupLocation, Location dropLocation,
+                    Double weightKg, LocalDate scheduledDate) {
+        this.vehicle = vehicle;
+        this.pickupLocation = pickupLocation;
+        this.dropLocation = dropLocation;
+        this.weightKg = weightKg;
+        this.scheduledDate = scheduledDate;
+    }
+
+   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Location getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public void setPickupLocation(Location pickupLocation) {
+        this.pickupLocation = pickupLocation;
+    }
+
+    public Location getDropLocation() {
+        return dropLocation;
+    }
+
+    public void setDropLocation(Location dropLocation) {
+        this.dropLocation = dropLocation;
+    }
+
+    public Double getWeightKg() {
+        return weightKg;
+    }
+
+    public void setWeightKg(Double weightKg) {
+        this.weightKg = weightKg;
+    }
+
+    public LocalDate getScheduledDate() {
+        return scheduledDate;
+    }
+
+    public void setScheduledDate(LocalDate scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+}
