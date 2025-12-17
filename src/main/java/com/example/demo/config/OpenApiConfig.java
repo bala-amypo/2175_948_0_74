b04e79212cf3425735ca0ddebbf11f0ab847387d
bala-amypo <bala@amypo.in>
2025-12-17 08:20:18 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9552.pro604cr.amypo.ai/")
+                        new Server().url("https://9169.pro604cr.amypo.ai/")
                 ));
         }
 }
---
+++ src/main/java/com/example/demo/repository/ShipmentRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.Shipment;
+
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+    List<Shipment> findByVehicleId(Long vehicleId);
+}
