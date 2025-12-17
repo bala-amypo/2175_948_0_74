81d3a98561a66f09475f881bf00566c6dff80b56
bala-amypo <bala@amypo.in>
2025-12-17 05:55:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -1,9 +1,87 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "")
+@Table(name = "route_optimization_results")
 public class RouteOptimizationResult {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shipment_id", nullable = false)
+    private Shipment shipment;
+
+    @Column(nullable = false)
+    private Double optimizedDistanceKm;
+
+    @Column(nullable = false)
+    private Double estimatedFuelUsageL;
+
+    @Column(nullable = false)
+    private LocalDateTime generatedAt;
+
+    
+    public RouteOptimizationResult() {}
+
+    
+    public RouteOptimizationResult(Shipment shipment,
+                                   Double optimizedDistanceKm,
+                                   Double estimatedFuelUsageL) {
+        this.shipment = shipment;
+        this.optimizedDistanceKm = optimizedDistanceKm;
+        this.estimatedFuelUsageL = estimatedFuelUsageL;
+    }
+
+    
+    @PrePersist
+    private void setGeneratedAt() {
+        this.generatedAt = LocalDateTime.now();
+    }
+
+   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Shipment getShipment() {
+        return shipment;
+    }
+
+    public void setShipment(Shipment shipment) {
+        this.shipment = shipment;
+    }
+
+    public Double getOptimizedDistanceKm() {
+        return optimizedDistanceKm;
+    }
+
+    public void setOptimizedDistanceKm(Double optimizedDistanceKm) {
+        this.optimizedDistanceKm = optimizedDistanceKm;
+    }
+
+    public Double getEstimatedFuelUsageL() {
+        return estimatedFuelUsageL;
+    }
+
+    public void setEstimatedFuelUsageL(Double estimatedFuelUsageL) {
+        this.estimatedFuelUsageL = estimatedFuelUsageL;
+    }
+
+    public LocalDateTime getGeneratedAt() {
+        return generatedAt;
+    }
+
+    public void setGeneratedAt(LocalDateTime generatedAt) {
+        this.generatedAt = generatedAt;
+    }
 }
